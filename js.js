9553aaaa0407049ad8b4a746949160be49bca40c
Jeremy Theobald <jtheo2011@gmail.com>
2022-09-13 19:44:54 -0600

    Ability to change input each round

--- js.js
+++ js.js
@@ -1,11 +1,7 @@
 let cpuStyle
 let capitalPlayerInput
-let cpuChoice = getComputerChoice()
-let playerInput = prompt("Rock, Paper, or Scissors?")
-let playerValue = convertInput(playerInput)
-let tie = `It was a tie! you chose ${capitalPlayerInput} and the cpu chose ${cpuStyle} but sadly ${capitalPlayerInput} does nothing to ${cpuStyle} :(` 
-let win = `You have won! you chose ${capitalPlayerInput} and the cpu chose ${cpuStyle} and ${capitalPlayerInput} demolishes weak little ${cpuStyle}`
-let lose = `You have lost! you chose ${capitalPlayerInput} and the cpu chose ${cpuStyle} and sadly your little ${capitalPlayerInput} has been destroyed by the cpu's ${cpuStyle}`
+let scorePlayer = 0
+let scoreCpu = 0
 
 function getComputerChoice() {
     let num = (Math.floor(Math.random() * 3)+1)
@@ -24,7 +20,7 @@ function getComputerChoice() {
 console.log(playerValue + " player")
 console.log(cpuStyle) */
 
-/* Converts player input into value */
+/* this function converts player input into value */
 function convertInput(string) {
     let newString = string.charAt(0).toUpperCase() + (string.slice(1).toLowerCase())
     capitalPlayerInput = newString
@@ -46,6 +42,9 @@ function convertInput(string) {
 /* 3 = scissors */
 function gameOne(player, cpu) {
     let decision
+    let tie = `It was a tie! you chose ${capitalPlayerInput} and the cpu chose ${cpuStyle} but sadly ${capitalPlayerInput} does nothing to ${cpuStyle} :(` 
+    let win = `You have won! you chose ${capitalPlayerInput} and the cpu chose ${cpuStyle} and ${capitalPlayerInput} demolishes weak little ${cpuStyle}`
+    let lose = `You have lost! you chose ${capitalPlayerInput} and the cpu chose ${cpuStyle} and sadly your little ${capitalPlayerInput} has been destroyed by the cpu's ${cpuStyle}`
     if (player == cpu) {
         decision = tie;
     } else if 
@@ -53,8 +52,10 @@ function gameOne(player, cpu) {
      (player == 2 && cpu == 3) ||
      (player == 3 && cpu == 1)) {
         decision = lose;
+        scoreCpu += 1;
     } else {
         decision = win;
+        scorePlayer += 1;
     }
     return decision;
 }
@@ -62,29 +63,23 @@ function gameOne(player, cpu) {
 /* console.log(gameOne(playerValue, cpuChoice)) */
 
 function game(times) {
-    function gameOne(player, cpu) {
-        let decision
-        if (player == cpu) {
-            decision = tie;
-        } else if 
-        ((player == 1 && cpu == 2) ||
-         (player == 2 && cpu == 3) ||
-         (player == 3 && cpu == 1)) {
-            decision = lose;
-        } else {
-            decision = win;
-        }
-        return decision;
-    }
     for (i = 1; i <= times; i++) {
+        let playerInput = prompt(`Game ${i}: Rock, Paper, or Scissors? Current score ${scorePlayer}/5`)
+        let playerValue = convertInput(playerInput)
+        let cpuChoice = getComputerChoice();
+        
         console.log(`game ${i}:`);
         console.log(gameOne(playerValue, cpuChoice));
-        cpuChoice = getComputerChoice();
     }
-    return `You just played ${times} games of Rock-Paper-Scissors. Hopefully you won the majority!`;
-    /* Add in a counter that counts how many games you won and decides if you won the majority */
-    /* Add in prompt to change the amount of games played */
-    /* add in way to change prompt every round */
+    if (scorePlayer > scoreCpu) {
+        return `You have won! You won ${scorePlayer}/${i} games`;
+    } else if (scorePlayer < scoreCpu) {
+        return `You have lost all of your honor and let the machines win! How could could you only win ${scorePlayer}/${i} games?`;
+    } else {
+        return `You tied? How could you have done such a thing. You need to try again.`;
+    }
+    
 } 
+
 
 console.log(game(5))
