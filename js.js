4b9a82a0461ec5753d9d2f3f95662c4b2a393498
Jeremy <jtheo2011@gmail.com>
2022-11-14 15:05:06 -0700

    Creation of physical buttons to interact with

--- js.js
+++ js.js
@@ -1,7 +1,77 @@
 let cpuStyle
-let capitalPlayerInput
-let scorePlayer = 0
-let scoreCpu = 0
+// let capitalPlayerInput
+// let scorePlayer = 0
+// let scoreCpu = 0
+
+
+
+// /* test values */
+// /* console.log(cpuChoice + " cpu")
+// console.log(playerValue + " player")
+// console.log(cpuStyle) */
+
+// /* this function converts player input into value */
+// function convertInput(string) {
+//     let newString = string.charAt(0).toUpperCase() + (string.slice(1).toLowerCase())
+//     capitalPlayerInput = newString
+//     let value
+//     if (newString == "Rock") {
+//         value = 1;
+//     } else if (newString == "Paper") {
+//         value = 2;
+//     } else if (newString == "Scissors") {
+//         value = 3;
+//     } else {
+//         alert("Invalid entry")
+//     }
+//     return value;
+// }
+
+// /* 1 = rock */
+// /* 2 = paper */
+// /* 3 = scissors */
+// function gameOne(player, cpu) {
+//     let decision
+//     let tie = `It was a tie! you chose ${capitalPlayerInput} and the cpu chose ${cpuStyle} but sadly ${capitalPlayerInput} does nothing to ${cpuStyle} :(` 
+//     let win = `You have won! you chose ${capitalPlayerInput} and the cpu chose ${cpuStyle} and ${capitalPlayerInput} demolishes weak little ${cpuStyle}`
+//     let lose = `You have lost! you chose ${capitalPlayerInput} and the cpu chose ${cpuStyle} and sadly your little ${capitalPlayerInput} has been destroyed by the cpu's ${cpuStyle}`
+//     if (player == cpu) {
+//         decision = tie;
+//     } else if 
+//     ((player == 1 && cpu == 2) ||
+//      (player == 2 && cpu == 3) ||
+//      (player == 3 && cpu == 1)) {
+//         decision = lose;
+//         scoreCpu += 1;
+//     } else {
+//         decision = win;
+//         scorePlayer += 1;
+//     }
+//     return decision;
+// }
+
+// /* console.log(gameOne(playerValue, cpuChoice)) */
+
+// function game(times) {
+//     for (i = 1; i <= times; i++) {
+//         let playerInput = prompt(`Game ${i}: Rock, Paper, or Scissors? Current score ${scorePlayer}/5`)
+//         let playerValue = convertInput(playerInput)
+//         let cpuChoice = getComputerChoice();
+        
+//         console.log(`game ${i}:`);
+//         console.log(gameOne(playerValue, cpuChoice));
+//     }
+//     if (scorePlayer > scoreCpu) {
+//         return `You have won! You won ${scorePlayer}/${i} games`;
+//     } else if (scorePlayer < scoreCpu) {
+//         return `You have lost all of your honor and let the machines win! How could could you only win ${scorePlayer}/${i} games?`;
+//     } else {
+//         return `You tied? How could you have done such a thing. You need to try again.`;
+//     }
+    
+// } 
+// console.log(game(5))
+
 
 function getComputerChoice() {
     let num = (Math.floor(Math.random() * 3)+1)
@@ -10,76 +80,46 @@ function getComputerChoice() {
     } else if (num == 2) {
         cpuStyle = "Paper"
     } else {
-        cpuStyle = "Scissors"
+        cpuStyle = "Scissors"  
     }
-    return num
+    return cpuStyle;
 }
 
-/* test values */
-/* console.log(cpuChoice + " cpu")
-console.log(playerValue + " player")
-console.log(cpuStyle) */
+let playerInput;
 
-/* this function converts player input into value */
-function convertInput(string) {
-    let newString = string.charAt(0).toUpperCase() + (string.slice(1).toLowerCase())
-    capitalPlayerInput = newString
-    let value
-    if (newString == "Rock") {
-        value = 1;
-    } else if (newString == "Paper") {
-        value = 2;
-    } else if (newString == "Scissors") {
-        value = 3;
-    } else {
-        alert("Invalid entry")
-    }
-    return value;
-}
+const buttons = document.querySelectorAll('button');
 
-/* 1 = rock */
-/* 2 = paper */
-/* 3 = scissors */
-function gameOne(player, cpu) {
-    let decision
-    let tie = `It was a tie! you chose ${capitalPlayerInput} and the cpu chose ${cpuStyle} but sadly ${capitalPlayerInput} does nothing to ${cpuStyle} :(` 
-    let win = `You have won! you chose ${capitalPlayerInput} and the cpu chose ${cpuStyle} and ${capitalPlayerInput} demolishes weak little ${cpuStyle}`
-    let lose = `You have lost! you chose ${capitalPlayerInput} and the cpu chose ${cpuStyle} and sadly your little ${capitalPlayerInput} has been destroyed by the cpu's ${cpuStyle}`
-    if (player == cpu) {
-        decision = tie;
-    } else if 
-    ((player == 1 && cpu == 2) ||
-     (player == 2 && cpu == 3) ||
-     (player == 3 && cpu == 1)) {
-        decision = lose;
-        scoreCpu += 1;
-    } else {
-        decision = win;
-        scorePlayer += 1;
-    }
-    return decision;
-}
-
-/* console.log(gameOne(playerValue, cpuChoice)) */
-
-function game(times) {
-    for (i = 1; i <= times; i++) {
-        let playerInput = prompt(`Game ${i}: Rock, Paper, or Scissors? Current score ${scorePlayer}/5`)
-        let playerValue = convertInput(playerInput)
-        let cpuChoice = getComputerChoice();
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        // console.log(button.id);
+        playerInput = button.id;
+        console.log('p chose: ' + playerInput);
+        console.log(winner());
         
-        console.log(`game ${i}:`);
-        console.log(gameOne(playerValue, cpuChoice));
-    }
-    if (scorePlayer > scoreCpu) {
-        return `You have won! You won ${scorePlayer}/${i} games`;
-    } else if (scorePlayer < scoreCpu) {
-        return `You have lost all of your honor and let the machines win! How could could you only win ${scorePlayer}/${i} games?`;
+    })
+})
+
+function winner(player, cpu) {
+    cpu = getComputerChoice();
+    player = playerInput;
+    console.log('cpu chose ' + cpu);
+    if (cpu == playerInput) {
+        return 'tie';
+    } else if ((cpu == 'Rock' && player == 'Scissors') || (cpu == 'Paper' && player == 'Rock') || (cpu == 'Scissors' && player == 'Paper')) {
+        return 'lose';
     } else {
-        return `You tied? How could you have done such a thing. You need to try again.`;
+        return 'win';
     }
-    
-} 
+}
 
+// const btn = document.querySelectorAll('#options');
+// btn.addEventListener('click', (e) => {
+//     console.log(e.target);
+//     // btn.classList.add('clicked');
+// })
 
-console.log(game(5))
+// document.addEventListener('click', (e) => {
+//     console.log(e.target);
+//     console.log(playerInput);
+// }
+// )
